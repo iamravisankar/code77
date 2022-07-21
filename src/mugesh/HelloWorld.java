592b6ef9e08bf9e4ecfc49be4b63ed0740ea1060
MugeshKP <107920052+MugeshKP@users.noreply.github.com>
2022-07-21 15:29:03 +0530

    5th commit

--- src/mugesh/HelloWorld.java
+++ src/mugesh/HelloWorld.java
@@ -4,11 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
-		System.out.println("mugesh");
-=======
-		System.out.println("hello world"); // manually add 
->>>>>>> 6b1d7d6ed1aa67808fb92db3470b5058d9d01a69
+		System.out.println("to learn pull"); // manually add 
 
 	}
 
