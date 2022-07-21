3419f59db578a35a2ce892db10399d6520b4ce17
VRManojKarthik <107391945+VRManojKarthik@users.noreply.github.com>
2022-07-21 18:27:43 +0530

    name change

--- src/Manoj/NewClass.java
+++ src/Manoj/NewClass.java
@@ -6,9 +6,9 @@ public class NewClass {
 	 String name2 = "ramesh";
 
          int num = 1;
-         int num = 2;
+         int num3 = 2;
 	String name3 = "Karthik";
-
+    String manoj = "ManojKarthik";
 
 	int n = 89;
 	String name5 = "123";
