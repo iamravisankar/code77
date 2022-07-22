3f82f0c08d88aa7954b6b51e563966c301595214
VRManojKarthik <107391945+VRManojKarthik@users.noreply.github.com>
2022-07-22 14:03:20 +0530

    add data type

--- src/Manoj/SyntaxJava.java
+++ src/Manoj/SyntaxJava.java
@@ -1,11 +1,14 @@
 package Manoj;
 
 public class SyntaxJava {
+
 	public static void main(String[] args) {
+		int a = 7;
 		System.out.println("Hello world");
-		System.out.println(1+1);
+		System.out.println(1 + 1);
 		System.out.print("welcome");
 		System.out.println("Java");
+		System.out.println(a);
 	}
 
 }
