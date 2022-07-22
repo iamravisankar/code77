3d8955958cc6d79d77991bee374adb2d017f1905
VRManojKarthik <107391945+VRManojKarthik@users.noreply.github.com>
2022-07-22 12:35:00 +0530

    change number 2+1 to 3+5

--- src/Manoj/JavaSyntax.java
+++ src/Manoj/JavaSyntax.java
@@ -5,7 +5,7 @@ public class JavaSyntax {
 	public static void main(String[] args) {
 		// The Println() Method
 		System.out.println("Welcome to Java Class");
-		System.out.println(2 + 1);
+		System.out.println(3 + 5);
 		System.out.println("Hello world!");
 		// The Print() Method words print on same line.
 		System.out.print("Java ");
