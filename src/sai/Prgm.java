75f940aefed5c2483dfaada3a16f99ee08631f63
Sushmithasanthakumar <108661820+Sushmithasanthakumar@users.noreply.github.com>
2022-07-22 15:47:58 +0530

    commit

---
+++ src/sai/Prgm.java
@@ -0,0 +1,25 @@
+package sai;
+
+public class Prgm {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int age  = 10;
+		 int age1 = 15;
+		 if (age < 10) {
+			 System.out.println("class a age " + age);
+			 
+		 }
+		 
+		 else if(age > 15) {
+			 System.out.println("class b  age" + age1);
+		 }
+		 else {
+			 System.out.println("no one");
+		 }
+			 
+
+
+	}
+
+}
