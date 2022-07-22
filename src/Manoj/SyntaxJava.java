a9173d5a0d2970a6ab362e3a72591575718bf58a
VRManojKarthik <107391945+VRManojKarthik@users.noreply.github.com>
2022-07-22 13:56:26 +0530

    new syntax

---
+++ src/Manoj/SyntaxJava.java
@@ -0,0 +1,11 @@
+package Manoj;
+
+public class SyntaxJava {
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		System.out.println(1+1);
+		System.out.print("welcome");
+		System.out.println("Java");
+	}
+
+}
