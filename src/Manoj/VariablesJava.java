e0e5f22a2787df29d8073f0dd2b98771b2c830e5
VRManojKarthik <107391945+VRManojKarthik@users.noreply.github.com>
2022-07-22 17:58:57 +0530

    Tamilnadu

---
+++ src/Manoj/VariablesJava.java
@@ -0,0 +1,22 @@
+package Manoj;
+
+public class VariablesJava {
+	public static void main(String[] args) {
+
+		String State = "TamilNadu";
+		int districts = 37;
+		districts = 38;
+		boolean tirupati = false;
+		float Peak = 8650.0f;
+		char letter = 'அ';
+		String world = "தமிழ்";
+
+		System.out.println("World's first language : " + world);
+		System.out.println("Tamil Letters starts from : " + letter);
+		System.out.println("In " + State + " there are " + districts + ":Districts");
+		System.out.println("Tirupati is in Tamilnadu State - " + tirupati);
+		System.out.println("Highest Mountain Peak in Tamilnadu is 'Doddabetta' in Ooty and height is - " + Peak);
+
+	}
+
+}
