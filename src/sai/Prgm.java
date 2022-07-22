d86ef225edce8241478e17d847d5dc81f5ffef98
Sushmithasanthakumar <108661820+Sushmithasanthakumar@users.noreply.github.com>
2022-07-22 15:56:04 +0530

    changed

--- src/sai/Prgm.java
+++ src/sai/Prgm.java
@@ -5,14 +5,14 @@ public class Prgm {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int age  = 10;
-		 int age1 = 15;
+		 
 		 if (age < 10) {
 			 System.out.println("class a age " + age);
 			 
 		 }
 		 
 		 else if(age > 15) {
-			 System.out.println("class b  age" + age1);
+			 System.out.println("class b  age" + age);
 		 }
 		 else {
 			 System.out.println("no one");
