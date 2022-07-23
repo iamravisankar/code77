23ee3493e6955811b373cbb3982d76176654b954
MugeshKP <mugesh065@gmail.com>
2022-07-23 10:35:45 +0530

    c4

--- src/Manoj/NewClass.java
+++ src/Manoj/NewClass.java
@@ -3,14 +3,6 @@ package Manoj;
 public class NewClass {
 
 	 String name = "manoj";
-	 String name2 = "ramesh";
-
-         int num = 1;
-         int num3 = 2;
-	String name3 = "Karthik";
-    String manoj = "ManojKarthik";
-
-	int n = 89;
-	String name5 = "123";
+	 String name10 = "karthik";
 
 }
