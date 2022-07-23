70b7518b3679889ed4fad52962a7977b9183811f
MugeshKP <mugesh065@gmail.com>
2022-07-23 10:47:08 +0530

    c4

--- src/mugesh/HelloWorld.java
+++ src/mugesh/HelloWorld.java
@@ -4,8 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String a="learn pull";
-		int a = 9;
+		String a="learn pull";		
 		int b =5;
 		System.out.println("to learn pull"); // manually add 
 
