da8f00afd786c2874bd185f761b79df41599aa45
subabose6 <subabose6@gmail.com>
2022-07-25 12:43:29 +0530

    switchcase

--- src/priya/SwitchCaseJava.java
+++ src/priya/SwitchCaseJava.java
@@ -1,10 +1,53 @@
 package priya;
+import java.util.Scanner;
 
 public class SwitchCaseJava {
+			static Scanner in = new Scanner(System.in);
+			public static void main(String[] args) {
+				//we have to enter the a and b value 
+				//it print the first value is belongs to in which case
+				
+				int a, b;
+				System.out.println("Enter a and b");
+				
+				a = in.nextInt();
+				b = in.nextInt();
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+				switch (b) {
 
-	}
+				case 1:
+					switch (a) {
+					case 1:
+						System.out.println("b is 1");
+						break;
 
-}
+					case 2:
+						System.out.println("b is 2");
+						break;
+
+					case 3:
+						System.out.println("b is 3");
+						break;
+					}
+					break;
+
+				case 2:
+					System.out.println("a is 2");
+					break;
+
+				case 3:
+					System.out.println("a is 3");
+					break;
+
+				default:
+					System.out.println("default statement here");
+					break;
+				}
+
+			}
+
+		}
+
+	
+
+
