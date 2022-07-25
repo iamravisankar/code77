364a8e59128d21caa9827b279155773b2d42a6c7
Sushmithasanthakumar <108661820+Sushmithasanthakumar@users.noreply.github.com>
2022-07-25 13:29:03 +0530

    prgm Cod 33 (#29)
    * commit
    
    * changed
    
    * add
    
    * prgm

---
+++ src/sushmi/Bating1.java
@@ -0,0 +1,13 @@
+package sushmi;
+import sushmi.Bating;
+
+public class Bating1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Bating myObj = new Bating();
+		myObj.mybating();
+
+	}
+
+}
