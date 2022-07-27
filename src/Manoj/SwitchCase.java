33d087c151fa2c98b5479ff5ede31ef3063554ce
VRManojKarthik <107391945+VRManojKarthik@users.noreply.github.com>
2022-07-27 12:17:23 +0530

    switch

--- src/Manoj/SwitchCase.java
+++ src/Manoj/SwitchCase.java
@@ -3,7 +3,7 @@ package Manoj;
 public class SwitchCase {
 	public static void main(String[] args) {
 		int day = 2;
-		
+
 		String dayString;
 		String dayType;
 
@@ -20,23 +20,23 @@ public class SwitchCase {
 		case 3:
 			dayString = "Wednesday";
 			break;
-			
+
 		case 4:
 			dayString = "Thursday";
 			break;
-			
+
 		case 5:
 			dayString = "Friday";
 			break;
-			
+
 		case 6:
 			dayString = "Saturday";
 			break;
-			
+
 		case 7:
 			dayString = "Sunday";
 			break;
-			
+
 		default:
 			dayString = "Invalid day";
 		}
