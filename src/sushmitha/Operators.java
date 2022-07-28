9b1b3a40653c0722b03b24b4f63192428ee25f94
Sushmithasanthakumar <108661820+Sushmithasanthakumar@users.noreply.github.com>
2022-07-28 20:26:34 +0530

    COD-58 (#38)
    * opera
    
    * Update .gitignore

---
+++ src/sushmitha/Operators.java
@@ -0,0 +1,23 @@
+package sushmitha;
+
+public class Operators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "sai";
+		int age = 10;
+		age += 13;
+		float per = 98.99f;
+		boolean res = true;
+		char grade = 'a';
+		int x = 100;
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println(per);
+		System.out.println(res);
+		System.out.println(grade);
+		System.out.println(x > 98 || x < 99);
+
+	}
+
+}
