d6ab57c2f93812e9f18d866507a4434a8f02ff8a
mugesh012 <mugeshmugesh35@gmail.com>
2022-07-29 10:17:56 +0530

    arraylist

---
+++ src/mugesh012/ArrayClass.java
@@ -0,0 +1,19 @@
+package mugesh012;
+
+public class ArrayClass {
+	String name;
+	int age;
+	boolean isMale;
+	public ArrayClass(String name, int age, boolean isMale) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.isMale = isMale;
+	}
+	@Override
+	public String toString() {
+		return "ArrayClass [name=" + name + ", age=" + age + ", isMale=" + isMale + "]";
+	}
+	
+
+}
